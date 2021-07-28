9cac358e2dcc72e80df97e97839c10492f617fff
kesavamuthu <kesavamuthu77@gmail.com>
2021-07-28 15:44:34 +0000

    packages added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap-theme.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+  <main>
+    <div className="py-5 text-center">
+      <img className="d-block mx-auto mb-4" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/>
+      <h2>Checkout form</h2>
+      <p className="lead">Below is an example form built entirely with Bootstrap’s form controls. Each required form group has a validation state that can be triggered by attempting to submit the form without completing it.</p>
     </div>
+
+    <div className="row g-5">
+      <div className="col-md-5 col-lg-4 order-md-last">
+        <h4 className="d-flex justify-content-between align-items-center mb-3">
+          <span className="text-primary">Your cart</span>
+          <span className="badge bg-primary rounded-pill">3</span>
+        </h4>
+        <ul className="list-group mb-3">
+          <li className="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 className="my-0">Product name</h6>
+              <small className="text-muted">Brief description</small>
+            </div>
+            <span className="text-muted">$12</span>
+          </li>
+          <li className="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 className="my-0">Second product</h6>
+              <small className="text-muted">Brief description</small>
+            </div>
+            <span className="text-muted">$8</span>
+          </li>
+          <li className="list-group-item d-flex justify-content-between lh-sm">
+            <div>
+              <h6 className="my-0">Third item</h6>
+              <small className="text-muted">Brief description</small>
+            </div>
+            <span className="text-muted">$5</span>
+          </li>
+          <li className="list-group-item d-flex justify-content-between bg-light">
+            <div className="text-success">
+              <h6 className="my-0">Promo code</h6>
+              <small>EXAMPLECODE</small>
+            </div>
+            <span className="text-success">−$5</span>
+          </li>
+          <li className="list-group-item d-flex justify-content-between">
+            <span>Total (USD)</span>
+            <strong>$20</strong>
+          </li>
+        </ul>
+        </div>
+        </div>
+
+       
+  </main>
+  <footer className="my-5 pt-5 text-muted text-center text-small">
+    <p className="mb-1">© 2017–2021 Company Name</p>
+    <ul className="list-inline">
+      <li className="list-inline-item"><a href="#">Privacy</a></li>
+      <li className="list-inline-item"><a href="#">Terms</a></li>
+      <li className="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+  </footer>
+</div >
   );
 }
 
