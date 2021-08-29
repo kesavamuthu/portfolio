f6b74c67d9295f42b6f381f6447a382fff60f9e2
Gokulkumar M <gokulkumar2908@gmail.com>
2021-08-29 11:56:54 +0530

    added objects for separate users

---
+++ src/user_details.ts
@@ -0,0 +1,16 @@
+const user = [{
+    name: "Gokulkumar",
+    logo: "logo.png",
+    contactno: 9047234234,
+    introtext: "This is my first portfolio using React, Javascript, SCSS and Bootstrap with responsive layout",
+    video: "https://www.youtube.com/watch?v=f02mOEt11OQ",
+    navigation: ["Home", "Project","FAQ", "Contact", ]
+},
+{
+    name: "Kesav",
+    logo: "logo.png",
+    contactno: 8072386812,
+    introtext:"Welcome to my portfolio. I've 4 years of experience in software developing",
+    video: "https://www.youtube.com/embed/_luhn7TLfWU",
+    navigation: ["Home", "Project", "Contact", ]
+}]
