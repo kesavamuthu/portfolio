3884c632b37ec32845bf61b1be5eab177ea62742
gokul2908 <gokulkumar2908@gmail.com>
2024-11-19 23:25:00 +0530

    Updated

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,182 +1,183 @@
-import axios from 'axios';
-import React, { ReactElement, useEffect, useRef, useState } from 'react';
-import { Repos } from '../../pages/homepage/userModel';
-import './style.scss';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Container, Grid } from '@mui/material';
-import { animated, useSpring } from '@react-spring/web';
+import axios from "axios";
+import React, { ReactElement, useEffect, useRef, useState } from "react";
+import { Repos } from "../../pages/homepage/userModel";
+import "./style.scss";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Container, Grid } from "@mui/material";
+import { animated, useSpring } from "@react-spring/web";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 
 interface Props {
-    name?: string;
-    projects?: string[][];
-    filteredProjects?: string[];
-    projectInfo:[{}];
-    openSourceProjectsInfo: {[type: string]: any};
+  name?: string;
+  projects?: string[][];
+  filteredProjects?: string[];
+  projectInfo: [{}];
+  openSourceProjectsInfo: { [type: string]: any };
 }
 
+const showPercentageInView = 0.01;
+
 export default function Projects({
-    name: profileName,
-    projects,
-    filteredProjects,
-    projectInfo,
-    openSourceProjectsInfo
+  name: profileName,
+  projects,
+  filteredProjects,
+  projectInfo,
+  openSourceProjectsInfo,
 }: Props): ReactElement {
-    const [gitRepos, setGitRepos] = useState<Repos[] | string[][]>();
-    const ref = useRef(null);
-    console.log('projectInfo:', projectInfo);
+  const [gitRepos, setGitRepos] = useState<Repos[] | string[][]>();
+  const ref = useRef(null);
+  console.log("projectInfo:", projectInfo);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const { data } = await axios.get(
-                    `https://api.github.com/users/${profileName}/repos`
-                );
-                setGitRepos(
-                    profileName
-                        ? (data as Repos[]).filter(
-                            ({ name }) => !filteredProjects?.includes(name)
-                        )
-                        : projects
-                );
-            } catch (error: any) {
-                alert(error?.message);
-            }
-        })();
-    }, [profileName, filteredProjects, projects]);
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(
+          `https://api.github.com/users/${profileName}/repos`
+        );
+        setGitRepos(
+          profileName
+            ? (data as Repos[]).filter(
+                ({ name }) => !filteredProjects?.includes(name)
+              )
+            : projects
+        );
+      } catch (error: any) {
+        alert(error?.message);
+      }
+    })();
+  }, [profileName, filteredProjects, projects]);
 
-    return (
-        <Container maxWidth='lg'>
-            <Grid
-                container
-                spacing={2}
-                columns={12}
-                justifyContent='space-evenly'
-            >
-                {projectInfo.map((info, i) => <Project key={i} {...info}/>)}
-                {(gitRepos as Repos[])?.map(
-                    ({ id, name, owner: { avatar_url, html_url } }, idx) => (
-                        <Project
-                            {...{
-                                id,
-                                name,
-                                avatar_url,
-                                html_url,
-                                ref,
-                                description: openSourceProjectsInfo[name],
-                                idx,
-                            }}
-                        />
-                    )
-                )}
-            </Grid>
-        </Container>
-    );
+  return (
+    <Container maxWidth="lg">
+      <Grid container spacing={2} columns={12} justifyContent="space-evenly">
+        {projectInfo.map((info, i) => (
+          <Project key={i} {...info} />
+        ))}
+        {(gitRepos as Repos[])?.map(
+          ({ id, name, owner: { avatar_url, html_url } }, idx) => (
+            <Project
+              {...{
+                id,
+                name,
+                avatar_url,
+                html_url,
+                ref,
+                description: openSourceProjectsInfo[name],
+                idx,
+              }}
+            />
+          )
+        )}
+      </Grid>
+    </Container>
+  );
 }
 
 function Project({
-    name,
-    description,
-    avatar_url,
-    html_url,
-    ref,
-    openSourceProjectsInfo,
-    idx,
+  name,
+  description,
+  avatar_url,
+  html_url,
+  ref,
+  openSourceProjectsInfo,
+  idx,
 }: any): ReactElement {
-    console.log(openSourceProjectsInfo);
-    const [springs, api] = useSpring(() => ({
-        from: { x: -1200 },
-    }));
-    const divRef = useRef(null);
-    const animationDone = useRef({ l2r: false, r2l: false });
+  console.log(openSourceProjectsInfo);
+  const divRef = useRef(null);
+  const animationDone = useRef({ l2r: false, r2l: false });
 
-    useEffect(() => {
-        const isElementInView = (el: any) => {
-            const rect = el?.getBoundingClientRect();
-            console.log({ rect, idx });
-            return (
-                rect.top >= 0 &&
-                rect.bottom - 5 <=
-                (window.innerHeight ||
-                    document.documentElement.clientHeight)
-            );
-        };
-        const handleScroll = () => {
-            const { l2r, r2l }: any = animationDone.current;
-            if (divRef.current && isElementInView(divRef.current)) {
-                if (!l2r || r2l) {
-                    animationDone.current.l2r = true;
-                    animationDone.current.r2l = false;
-                    api.start({
-                        from: {
-                            x: -1200,
-                        },
-                        to: {
-                            x: 100,
-                        },
-                    });
-                }
-            } else {
-                if (!r2l) {
-                    animationDone.current.r2l = true;
-                    animationDone.current.l2r = false;
-                    api.start({
-                        from: {
-                            x: 100,
-                        },
-                        to: {
-                            x: -1200,
-                        },
-                    });
-                }
-            }
-        };
+  const { scrollYProgress } = useScroll({
+    target: divRef,
+    offset: ["end end", "start start"],
+  });
 
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, [api, idx]);
+  const [springs, api] = useSpring(() => ({
+    from: { x: -1200 },
+  }));
 
-    return (
-        <animated.div
-            style={{
-                ...springs,
-            }}
-            ref={divRef}
-        >
-            <Grid item xs={12} sm={6} key={idx}>
-                <Card sx={{ minWidth: 345 }} className=' my-3'>
-                    <CardMedia
-                        sx={{ height: 140 }}
-                        image='https://www.uib.no/sites/w3.uib.no/files/styles/content_main/public/media/colourbox3117235_no5859_edit.jpg?itok=kPbJVL51'
-                        title='green iguana'
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant='h5' component='div'>
-                            {name}
-                        </Typography>
-                        <Typography variant='body2' color='text.secondary'>
-                            {description || 'No description'}
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <Button size='small'>Share</Button>
-                        {!!html_url && <Button
-                            size='small'
-                            onClick={() =>
-                                window.open(html_url + '/' + name, '_blank')
-                            }
-                        >
-                            Learn More
-                        </Button>}
-                    </CardActions>
-                </Card>
-            </Grid>
-        </animated.div>
-    );
+  useEffect(() => {
+    const isElementInView = () => {
+      console.log(scrollYProgress, scrollYProgress.get(), divRef);
+
+      return Number(scrollYProgress.get()) > showPercentageInView;
+    };
+    const unsubscribe = scrollYProgress.on("change", () => {
+      const { l2r, r2l }: any = animationDone.current;
+      if (isElementInView()) {
+        if (!l2r || r2l) {
+          animationDone.current.l2r = true;
+          animationDone.current.r2l = false;
+          api.start({
+            from: {
+              x: -1200,
+            },
+            to: {
+              x: 0,
+            },
+          });
+        }
+      } else {
+        if (!r2l) {
+          animationDone.current.r2l = true;
+          animationDone.current.l2r = false;
+          api.start({
+            from: {
+              x: 0,
+            },
+            to: {
+              x: -1200,
+            },
+          });
+        }
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, [scrollYProgress]);
+
+  return (
+    <>
+      <animated.div
+        style={{
+          ...springs,
+        }}
+      >
+        <Grid item xs={12} sm={6} key={idx} ref={divRef}>
+          <Card sx={{ minWidth: 345 }} className=" my-3">
+            <CardMedia
+              sx={{ height: 140 }}
+              image="https://www.uib.no/sites/w3.uib.no/files/styles/content_main/public/media/colourbox3117235_no5859_edit.jpg?itok=kPbJVL51"
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {description || "No description"}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              {!!html_url && (
+                <Button
+                  size="small"
+                  onClick={() => window.open(html_url + "/" + name, "_blank")}
+                >
+                  Learn More
+                </Button>
+              )}
+            </CardActions>
+          </Card>
+        </Grid>
+      </animated.div>
+    </>
+  );
 }
--- src/user_details.ts
+++ src/user_details.ts
@@ -1,4 +1,4 @@
-const user = [
+const user: any = [
   {
     name: "gokul2908",
     logo: "./images/gokul_logo.png",
@@ -9,39 +9,38 @@ const user = [
     ],
     video: "https://www.youtube.com/watch?v=f02mOEt11OQ",
     navigation: ["Home", "Project", "FAQ", "Contact"],
-    projects: [
-      [
-        "title",
-        "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.",
-      ],
-    ],
+    projects: [],
     email: "gokulkumar2908@gmail.com",
     filteredProjects: ["ansys-fluent"],
     projectInfo: [{}],
     openSourceProjectsInfo: {
-'':null
-    }
+      "": null,
+    },
   },
   {
     name: "kesavamuthu",
     logo: "./images/kesav_logo.jpg",
     introText: [
       " ",
-      `My name is kesav. I've ${new Date().getFullYear() - 2017}+ years of experience in software development & team management...`,
+      `My name is kesav. I've ${
+        new Date().getFullYear() - 2017
+      }+ years of experience in software development & team management...`,
     ],
     video: "https://www.youtube.com/embed/_luhn7TLfWU",
     navigation: ["Home", "Project", "Contact"],
     projects: [["title", "content"]],
     email: "kesavamuthu77@gmail.com",
-    projectInfo: [{
-      name: 'Medium like blogging CMS app.',
-      description: "The CMS application, designed for both the Admin and User, was constructed within a single package. I, as a sole contributor, developed this application from start to finish, utilizing the outbox model to publish articles. The determination of various role-based permissions was achieved through the use of a Tree data structure and middleware. Additionally, dynamic loading was implemented to enhance the application's performance for end-users.",
-      techStack: ['Next js', 'Prisma', "React Js", "Js", "MUI"],
-    }],
+    projectInfo: [
+      {
+        name: "Medium like blogging CMS app.",
+        description:
+          "The CMS application, designed for both the Admin and User, was constructed within a single package. I, as a sole contributor, developed this application from start to finish, utilizing the outbox model to publish articles. The determination of various role-based permissions was achieved through the use of a Tree data structure and middleware. Additionally, dynamic loading was implemented to enhance the application's performance for end-users.",
+        techStack: ["Next js", "Prisma", "React Js", "Js", "MUI"],
+      },
+    ],
     openSourceProjectsInfo: {
-      '':null
-
-    }
+      "": null,
+    },
   },
 ];
 
